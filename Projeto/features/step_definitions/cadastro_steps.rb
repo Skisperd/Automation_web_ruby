7d64b2aa47612dc798a6adebf58cb31fce154f09
Tiago Dias <tf.dias.oliveira@gmail.com>
2021-07-13 16:04:20 -0400

    Update

--- Projeto/features/step_definitions/cadastro_steps.rb
+++ Projeto/features/step_definitions/cadastro_steps.rb
@@ -7,21 +7,16 @@ Quando('submeto o seguinte formulário de cadastro:') do |table|
     # table is a Cucumber::MultilineArgument::DataTable
     user = table.hashes.first   
 
-    MongoDB.new.remove_user(user[:email])
-<<<<<<< HEAD
-    
+    MongoDB.new.remove_user(user[:email])   
     @signup_page.create(user)
 end
-=======
 
-    find("#fullName").set user[:nome]
-    find("#email").set user[:email]
-    find("#password").set user[:senha]
->>>>>>> origin/main
 
 Quando('submeto as seguintes informações de cadastro:') do |table|
     # table is a Cucumber::MultilineArgument::DataTable
     user = table.hashes.first   
-    
+    find("#fullName").set user[:nome]
+    find("#email").set user[:email]
+    find("#password").set user[:senha]
     @signup_page.create(user)
 end
