f63289db60ce06f523dfff8cc6cbc0d4ac046f6c
Tiago Dias <tf.dias.oliveira@gmail.com>
2021-07-27 10:45:29 -0400

    Update correção de bugs

--- Projeto/features/support/views/components/alert.rb
+++ Projeto/features/support/views/components/alert.rb
@@ -1,5 +1,3 @@
-
-
 class Alert 
     include Capybara::DSL
 
--- Projeto/features/support/views/pages/dash_page.rb
+++ Projeto/features/support/views/pages/dash_page.rb
@@ -1,11 +1,10 @@
-
-
 class DashPage
     include Capybara::DSL
 
     def on_dashboard?
         page.has_css?(".dashboard")
     end
+    
     def goto_equipo_form
         click_button "Criar anúncio"
     end
